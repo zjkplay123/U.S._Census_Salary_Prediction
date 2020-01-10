24ec934ca0bc850d8c7e48e1f3b76453011f8981
Jinkai Zhang <56803578+zjkplay123@users.noreply.github.com>
2020-01-10 14:02:50 +0000

    census_income_prediction
    To predict the profiles of the people that make more than $50,000 / year using random forest.

---
+++ Census_Prediction.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jinkai Zhang
+
+Accuracy: 95.19%
+"""
+
+import pandas as pd
+import heapq
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+
+train = pd.read_csv('Desktop/census/us_census_full/us_census_full/census_income_learn.csv',header=None)
+test = pd.read_csv('Desktop/census/us_census_full/us_census_full/census_income_test.csv',header=None)
+
+#check the missing value
+train.isnull().any()
+
+#results suggest all missing value is represented as ' ?'
+#I do not drop missing value since the large percentage accounted. Instead, it is viewd as 'Missing'
+train.replace(to_replace = ' ?', value = 'Missing', inplace = True)
+test.replace(to_replace = ' ?', value = 'Missing', inplace = True)
+
+#Descriptive statistics and check the missing value
+for col in train.columns:
+    print(train[col].value_counts())
+
+#numeric columns
+num_column = [0,2,3,16,17,18,24,30,36,38,39,40,41]
+
+'''
+#describe the variable distribution, the extreme values and so on
+num_data = train[num_column]
+num_data.describe()
+'''
+
+#non_numeric columns
+non_num_train = train.drop(columns = num_column)
+non_num_column = non_num_train.columns
+
+non_num_test = test.drop(columns = num_column)
+non_num_column = non_num_test.columns
+
+#convert non_numeric to numeric (e.g. 0, 1, 2, 3,...)
+def get_convert (x, non_num_column):
+    for i in non_num_column:
+        array = list(x[i].unique())
+        x[i] = x[i].map(lambda y: array.index(y))     
+        print (i)    
+    return x        
+
+con_num_train = get_convert(non_num_train, non_num_column)
+con_num_test = get_convert(non_num_test, non_num_column)
+
+
+train = pd.concat( [con_num_train, train[num_column]], axis = 1 )
+test = pd.concat( [con_num_test, test[num_column]], axis = 1 )
+
+#split train and test data
+train_x = train.iloc[:, :41]
+train_y = train.iloc[:, 41]
+
+test_x = test.iloc[:, :41]
+test_y = test.iloc[:, 41]
+
+#lable 1 and 0 denote ' 50000+.' and ' - 50000.'
+def get_class (x):
+    array = list(x.unique())
+    x = x.map(lambda y: array.index(y))
+    return x
+
+train_y = get_class(train_y)
+test_y = get_class(test_y)
+
+
+#scale the feature using min-max method
+scl = MinMaxScaler()
+scl.fit(train_x)
+train_x = pd.DataFrame(scl.transform(train_x),columns = train_x.columns)
+
+scl = MinMaxScaler()
+scl.fit(test_x)
+test_x = pd.DataFrame(scl.transform(test_x),columns = test_x.columns)
+
+#find best random forest model
+def Best_RF_Model (train_x, train_y, test_x, test_y):
+    min_samples_split = [5, 7]
+    max_depth = [15,20,25,30]
+    n_estimators = [10, 20, 50, 100]
+    score = 0
+    parameter = []
+    feature_importance = []
+    max_features = [7, 9, 12, 15]
+    for split in min_samples_split:
+        for depth in max_depth:
+            for num in n_estimators:
+                for fea in max_features:
+                    rf_model = RandomForestClassifier(n_estimators=num,min_samples_split = split,\
+                                max_features = fea, max_depth=depth, \
+                                  random_state=0,criterion='gini',oob_score=True)
+                    rf_model.fit (train_x, train_y)
+                    new_score = rf_model.score (test_x, test_y)
+                    if new_score > score:
+                        score = new_score
+                        parameter = [split,depth,num,fea]
+                        feature_importance = rf_model.feature_importances_
+                        print(score)
+                        print(parameter)
+    return  [score, parameter, feature_importance] 
+
+result = Best_RF_Model (train_x, train_y, test_x, test_y)
+
+
+#To save the running time, the best model is as follows
+rf_model = RandomForestClassifier(n_estimators = 100,min_samples_split = 7,\
+                                max_features = 15, max_depth = 20, \
+                                  random_state = 0 ,criterion = 'gini',oob_score = True)
+rf_model.fit(train_x, train_y)
+rf_model.score(test_x, test_y)
+rf_model.feature_importances_
+
+#find the largest feature orders
+largest_features = heapq.nlargest(3, rf_model.feature_importances_)
+first_index = list(rf_model.feature_importances_).index(largest_features[0])
+second_index = list(rf_model.feature_importances_).index(largest_features[1])
+third_index = list(rf_model.feature_importances_).index(largest_features[2])
+
+#find the largest features using the orders in data
+first_feature = train_x.columns[first_index]
+second_feature = train_x.columns[second_index]
+third_feature = train_x.columns[third_index]
+
+#The most important features are:
+#1. full or part time employment stat
+#2. capital losses
+#3. marital stat
+
+
+
+
+
+
+
